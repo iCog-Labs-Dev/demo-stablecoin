f99c1d38665dfa67d9b4b2fd416693d871a7d796
EyobAshenaki <ethioeyoba@gmail.com>
2023-06-12 11:11:33 +0300

    feat(NFT): implement NFT policy

--- src/on-chain/scripts/NFTPolicy.hs
+++ src/on-chain/scripts/NFTPolicy.hs
@@ -1,68 +1,77 @@
-{-# LANGUAGE DataKinds         #-}
-{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TemplateHaskell   #-}
-
-{-# OPTIONS_GHC -Wno-unused-imports #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# OPTIONS_GHC -Wno-unused-imports #-}
+
 {-# HLINT ignore "Use fewer imports" #-}
 
 module NFTPolicy where
 
-import           Plutus.V1.Ledger.Value     (flattenValue)
-import           Plutus.V2.Ledger.Api       (BuiltinData, CurrencySymbol,
-                                             MintingPolicy,
-                                             ScriptContext (scriptContextTxInfo),
-                                             TokenName (unTokenName),
-                                             TxId (TxId, getTxId),
-                                             TxInInfo (txInInfoOutRef),
-                                             TxInfo (txInfoInputs, txInfoMint),
-                                             TxOutRef (TxOutRef, txOutRefId, txOutRefIdx),
-                                             mkMintingPolicyScript, PubKeyHash)
-import qualified PlutusTx
-import           PlutusTx.Builtins.Internal (BuiltinByteString (BuiltinByteString))
-import           PlutusTx.Prelude           (Bool (False, True), Eq ((==)), any,
-                                             traceIfFalse, ($), (&&), (.))
-import           Prelude                    (IO, undefined)
-import           Utilities                  (bytesToHex, currencySymbol,
-                                             wrapPolicy, writeCodeToFile,
-                                             writePolicyToFile)
+import Plutus.V1.Ledger.Value (flattenValue)
+import Plutus.V2.Ledger.Api
+  ( BuiltinData,
+    CurrencySymbol,
+    MintingPolicy,
+    PubKeyHash,
+    ScriptContext (scriptContextTxInfo),
+    TokenName (unTokenName),
+    TxId (TxId, getTxId),
+    TxInInfo (txInInfoOutRef),
+    TxInfo (txInfoInputs, txInfoMint),
+    TxOutRef (TxOutRef, txOutRefId, txOutRefIdx),
+    mkMintingPolicyScript,
+  )
 import Plutus.V2.Ledger.Contexts (findTxInByTxOutRef)
-import PlutusTx.Prelude (Maybe(Just, Nothing))
+import qualified PlutusTx
+import PlutusTx.Builtins.Internal (BuiltinByteString (BuiltinByteString))
+import PlutusTx.Prelude (Bool (False, True), Eq ((==)), Maybe (Just, Nothing), any, traceIfFalse, ($), (&&), (.))
+import Utilities
+  ( bytesToHex,
+    currencySymbol,
+    wrapPolicy,
+    writeCodeToFile,
+    writePolicyToFile,
+  )
+import Prelude (IO, undefined)
+
 -- import PlutusTx (liftCode, applyCode)
 
-{-# INLINABLE mkNFTPolicy #-}
-
+{-# INLINEABLE mkNFTPolicy #-}
 mkNFTPolicy :: TxOutRef -> () -> ScriptContext -> Bool
-mkNFTPolicy oRef _ ctx =    traceIfFalse "The UTxO must be consumed" utxoConsumed &&
-                            traceIfFalse "Only 1 NFT can be minted" rightAmountMinted
-    where
-        info :: TxInfo
-        info = scriptContextTxInfo ctx
+mkNFTPolicy oRef _ ctx =
+  traceIfFalse "The UTxO must be consumed" utxoConsumed
+    && traceIfFalse "Only 1 NFT can be minted" rightAmountMinted
+  where
+    info :: TxInfo
+    info = scriptContextTxInfo ctx
 
-        utxoConsumed :: Bool
-        utxoConsumed = case findTxInByTxOutRef oRef info of
-                        Just _ -> True
-                        Nothing -> False
+    utxoConsumed :: Bool
+    utxoConsumed = case findTxInByTxOutRef oRef info of
+      Just _ -> True
+      Nothing -> False
 
-        rightAmountMinted :: Bool
-        rightAmountMinted = case flattenValue $ txInfoMint info of
-                            [( _ , _ , amt )] -> amt == 1
-                            _                 -> False
+    rightAmountMinted :: Bool
+    rightAmountMinted = case flattenValue $ txInfoMint info of
+      [(_, _, amt)] -> amt == 1
+      _ -> False
 
-{-# INLINABLE wrappedNFTPolicy #-}
-
+{-# INLINEABLE wrappedNFTPolicy #-}
 -- ======== Apply the parameters in lucid ========
-wrappedNFTPolicy :: BuiltinData -> BuiltinData -> BuiltinData -> BuiltinData -> ()
-wrappedNFTPolicy tid idx = wrapPolicy $ mkNFTPolicy oRef
-    where
-            oRef :: TxOutRef
-            oRef = TxOutRef (TxId $ PlutusTx.unsafeFromBuiltinData tid) (PlutusTx.unsafeFromBuiltinData idx)
+wrappedNFTPolicy :: BuiltinData -> BuiltinData -> BuiltinData -> BuiltinData -> BuiltinData -> ()
+wrappedNFTPolicy tid idx tn = wrapPolicy $ mkNFTPolicy oRef tn'
+  where
+    oRef :: TxOutRef
+    oRef = TxOutRef (TxId $ PlutusTx.unsafeFromBuiltinData tid) (PlutusTx.unsafeFromBuiltinData idx)
 
-compiledNFTCode :: PlutusTx.CompiledCode (BuiltinData -> BuiltinData -> BuiltinData -> BuiltinData -> ())
-compiledNFTCode = $$(PlutusTx.compile [|| wrappedNFTPolicy ||])
+    tn :: TokenName
+    tn' = PlutusTx.unsafeFromBuiltinData tn
 
-saveNFTCode :: IO()
+compiledNFTCode :: PlutusTx.CompiledCode (BuiltinData -> BuiltinData -> BuiltinData -> BuiltinData -> BuiltinData -> ())
+compiledNFTCode = $$(PlutusTx.compile [||wrappedNFTPolicy||])
+
+saveNFTCode :: IO ()
 saveNFTCode = writeCodeToFile "../assets/nft.plutus" compiledNFTCode
 
 -- ========== Apply the parameters while serializing the script =============
